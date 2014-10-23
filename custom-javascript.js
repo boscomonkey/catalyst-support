f581fca664fd1426dea29eddc9206482860dc7c5
Bosco So <bosco.so@gsa.gov>
2014-10-23 09:47:39 -0400

    adding appendJump function to eventually add a click handler to HREF

--- custom-javascript.js
+++ custom-javascript.js
@@ -1,21 +1,37 @@
-<script>
+//<script>
+
+
 $(document).ready(function(){
 
-// inject "Submit Video Pitch..." link if this is problems page
-//
-if(window.location.href.match( /catalyst.energy.gov.+discussionID=6952/ )) {
-var listItems = $(".idea");
-listItems.each(function(idx, li) {
-    var idea = $(li);
-    var ideaId = idea.attr('id');
+    function appendJump(jumpId) {
+        var href = window.location.href;
+        if (href.match(/#/)) {
+            return href.replace(/#.*/, '#' + jumpId);
+        }
+        else {
+            return href + '#' + jumpId;
+        }
+    }
 
-    // debug: console.log(idx, ':', 'idea ID', ideaId);
+    // inject "Submit Video Pitch..." link if this is problems page
+    //
+    if(window.location.href.match( /catalyst.energy.gov.+discussionID=6952/ )) {
+        var listItems = $(".idea");
+        listItems.each(function(idx, li) {
+            var idea = $(li);
+            var ideaId = idea.attr('id');
 
-    idea.find('.voting').append('<a class="nyroModal post" href="http://catalyst.energy.gov/a/showCreateTopic.do?idea='
+            // debug: console.log(idx, ':', 'idea ID', ideaId);
+
+            idea
+                .find('.voting')
+                .append('<a class="nyroModal post" href="http://catalyst.energy.gov/a/showCreateTopic.do?idea='
                         + ideaId
-                        + ' " style="position:absolute;top:200px;text-align:center">Submit Video Pitch Now!</a>');
-    $('.nyroModal').nyroModal();
+                        + ' " style="position:absolute;top:200px;text-align:center")>Submit Video Pitch Now!</a>');
+            $('.nyroModal').nyroModal();
+        });
+    }
 });
-}
-});
-</script>
+
+
+//</script>
