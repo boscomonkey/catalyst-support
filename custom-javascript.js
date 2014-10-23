7c18dca53c4ff9e56926e156f2e2221f7ec156e1
Bosco So <bosco.so@gsa.gov>
2014-10-23 09:41:24 -0400

    "Custom JavaScript" for popping up submission dialog with "idea ID"

---
+++ custom-javascript.js
@@ -0,0 +1,21 @@
+<script>
+$(document).ready(function(){
+
+// inject "Submit Video Pitch..." link if this is problems page
+//
+if(window.location.href.match( /catalyst.energy.gov.+discussionID=6952/ )) {
+var listItems = $(".idea");
+listItems.each(function(idx, li) {
+    var idea = $(li);
+    var ideaId = idea.attr('id');
+
+    // debug: console.log(idx, ':', 'idea ID', ideaId);
+
+    idea.find('.voting').append('<a class="nyroModal post" href="http://catalyst.energy.gov/a/showCreateTopic.do?idea='
+                        + ideaId
+                        + ' " style="position:absolute;top:200px;text-align:center">Submit Video Pitch Now!</a>');
+    $('.nyroModal').nyroModal();
+});
+}
+});
+</script>
