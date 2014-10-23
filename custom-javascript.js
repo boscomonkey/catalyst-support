db729ff22e81fb94c204cceaa7ad4b0e41b008df
Bosco So <bosco.so@gsa.gov>
2014-10-23 11:08:08 -0400

    attaching click handler to "Submit Video Pitch Now!" - updates page URL with jumpID

--- custom-javascript.js
+++ custom-javascript.js
@@ -21,14 +21,33 @@ $(document).ready(function(){
             var idea = $(li);
             var ideaId = idea.attr('id');
 
-            // debug: console.log(idx, ':', 'idea ID', ideaId);
+            // DEBUG: console.log(idx, ':', 'idea ID', ideaId);
 
-            idea
-                .find('.voting')
-                .append('<a class="nyroModal post" href="http://catalyst.energy.gov/a/showCreateTopic.do?idea='
-                        + ideaId
-                        + ' " style="position:absolute;top:200px;text-align:center")>Submit Video Pitch Now!</a>');
-            $('.nyroModal').nyroModal();
+            // create element to attach handlers
+            var eltHtml = '<a class="nyroModal post" href="http://catalyst.energy.gov/a/showCreateTopic.do?idea='
+                + ideaId
+                + ' " style="position:absolute;top:200px;text-align:center")>Submit Video Pitch Now!</a>';
+            var newElt = $(eltHtml);
+
+            // this handler adds "ideaId" to page URL
+            var fnAddJumpId = function() {
+                window.location.href = appendJump(ideaId);
+            };
+            // this handler removes any "ideaId" from page URL
+            var fnClearJumpId = function() {
+                var href = window.location.href;
+                if (href.match(/#/)) {
+                    window.location.href = href.replace(/#.*/, '');
+                }
+            };
+
+            // attach handlers to new element
+            //// DEBUG: newElt.hover(fnAddJumpId, fnClearJumpId);
+            newElt.click(fnAddJumpId);
+
+            // add new element into correct part of the DOM
+            idea.find('.voting').append(newElt);
+            newElt.nyroModal();
         });
     }
 });
